b058f30b720354634844923e694a104ad221bfaf
Snitch-1302 <114893622+Snitch-1302@users.noreply.github.com>
2026-07-17 08:17:03 +0530

    Refactor Tor proxy setup and cleanup

--- create_basic_tor_proxy.py
+++ create_basic_tor_proxy.py
@@ -1,27 +1,20 @@
-import io
 import os
-import stem.process
 import re
-import requests
-import json
-from datetime import datetime
+import stem.process
 
+from check_ip import check_current_ip
 
 SOCKS_PORT = 9050
-TOR_PATH = os.path.normpath(os.getcwd()+"\\tor\\tor.exe")
+TOR_PATH = os.path.normpath(os.getcwd() + "\\tor\\tor.exe")
+
 tor_process = stem.process.launch_tor_with_config(
-  config = {
-    'SocksPort': str(SOCKS_PORT),
-  },
-  init_msg_handler = lambda line: print(line) if re.search('Bootstrapped', line) else False,
-  tor_cmd = TOR_PATH
+    config={
+        'SocksPort': str(SOCKS_PORT),
+    },
+    init_msg_handler=lambda line: print(line) if re.search('Bootstrapped', line) else False,
+    tor_cmd=TOR_PATH
 )
 
+check_current_ip()
 
-PROXIES = {
-    'http': 'socks5://127.0.0.1:9050',
-    'https': 'socks5://127.0.0.1:9050'
-}
-response = requests.get("http://ip-api.com/json/", proxies=PROXIES)
-result = json.loads(response.content)
-print('TOR IP [%s]: %s %s'%(datetime.now().strftime("%d-%m-%Y %H:%M:%S"), result["query"], result["country"]))
+tor_process.kill()
